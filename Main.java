6ce1295ad1d22b152aaaec364f63963d04b96502
Şengül Kaya <sengulkaya15@yahoo.com>
2021-09-23 16:02:06 +0300

    çiftve4'ünkatlarıolansayılarıtoplayıpekranabasanprogram first commit

--- Main.java
+++ Main.java
@@ -1,38 +1,28 @@
 /*
- Artık yıl, Miladî takvimde 365 yerine 366 günü olan yıl. 
- Bu fazladan gün (artık gün), normalde 28 gün olan şubat ayına 29 Şubat’ın eklenmesi ile elde edilir.
- artık yıllar 4 rakamının katı olan yıllardır:
- 100'ün katı olan yıllardan sadece 400'e kalansız olarak bölünebilenler artık yıldır:
- if (year is not divisible by 4) then (it is a common year)
-else if (year is not divisible by 100) then (it is a leap year)
-else if (year is not divisible by 400) then (it is a common year)
-else (it is a leap year)
+Java döngüler ile tek bir sayı girilene kadar kullanıcıdan girişleri kabul eden
+ ve girilen değerlerden çift ve 4'ün katları olan sayıları toplayıp ekrana basan programı yazıyoruz.
+
+
  */
 package java101;
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		
+		int total = 0;
+		int n;
 		Scanner kb = new Scanner(System.in);
 		
-		System.out.println("Sayı giriniz : ");
-		int num = Integer.parseInt(kb.nextLine());
-		int i = 0;
-		int sum = 0;
-		int count = 0;
-		for (;;) {
-			if ((i % 3 == 0) && (i % 4 == 0)) {
-				sum += i;
-				count++;
+		do {
+			System.out.println("Sayı giriniz : ");
+			n = Integer.parseInt(kb.nextLine());
+			if (n % 4 == 0) {
+				total += n;
 			}
-				
 			
-			if (i == num)
-				break;
-			
-			i++;
-			}
-		System.out.printf("Average : %.2f%n", sum / (double)count);
+		} while (n % 2 == 0);
+		
+		System.out.printf("Total : %d%n", total);
 
 	}
 }
